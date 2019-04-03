205bef4ca01cba09fa8b80585b63512258cd79af
Fanny Mayorga <fes.mayorga@yavirac.edu.ec>
2019-04-03 15:42:15 -0500

    prueba 01

--- contavox/contavoxMachine03.py
+++ contavox/contavoxMachine03.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split           
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression          
 from sklearn.metrics import mean_squared_error,r2_score
 from math import sqrt    
 
@@ -11,11 +11,14 @@ data = pd.read_csv("C:/Users/admin/Documents/Fanny Mayorga/Python/csv/queue_call
 
 data['queue'] = data['queue'].replace(('Servicios Preferenciales','H_EAV','H_CCC','H_CIT','H_INF','H_AMB','H_MAD','M_UIO','H_HOS','H_ACT','H_DEN','M_GYE','H_VEN','M_SAC','M_COR','H_ECA'),1)
 
-data['time'] = data['time'].astype("datetime64[ns]")
-data['time'] = data['time'].map(lambda x: x.replace(second=0, minute=0))
+data['time'] = pd.to_datetime(data['time'])
+data['date'] = pd.to_datetime(data['date'])
 data['queue'] = data['queue'].astype("int64")
+data['time'] = data['time'].map(lambda x: x.replace(second=0, minute=0))
+data['time'] = data['time'].dt.hour
+data['date'] = data['date'].dt.day
 
- 
+
 data = data.groupby(['date','time']).sum()
 
 data.fillna(0, inplace=True)
@@ -30,23 +33,21 @@ before_rows - afther_rows
 
 data.reset_index(level=0, inplace=True)
 
-mask1 = data['date'] > '2017-01-01'
+mask1 = data['date'] > '2010-01-01'
 mask2 = data['date'] < '2019-03-27' 
 
 data = data[mask1 & mask2]
 
-data['date'] = pd.to_datetime(data['date'])
 data['date'] = data['date'].astype("int64")
 
-print(data.dtypes)
-
 features = ['queue']
+
 y = data[features]
+
 target = ['date']
+
 x = data[target]
 
-print(x.head())
-print(x.shape,y.shape)
 X_train, X_test ,  y_train , y_test = train_test_split(x,y , test_size=0.20, random_state=42)
 future = LinearRegression()
 future.fit(X_train, y_train)
@@ -55,14 +56,14 @@ y_prediction = future.predict(X_test).astype("int64")
 
 RMSE = sqrt(mean_squared_error(y_true= y_test,y_pred= y_test)) 
 
+data['date'] = data['date'].astype("datetime64[ns]")
+
 m = future.coef_[0]
 b = future.intercept_
 y_m = m*x+b
+
 print(y_prediction[:10])
-
-
-data['date'] = data['date'].astype("datetime64[ns]")
-print(data.head(11))
+print(data.head(9))
 
 plt.scatter(x,y)
 plt.plot(x,y_m, color='red')
@@ -71,3 +72,4 @@ plt.ylabel('queue')
 plt.title('calls per days')
 plt.grid()
 plt.show()
+
--- oye/prueba12.py
+++ oye/prueba12.py
@@ -1,21 +1,19 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split           
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression          
 from sklearn.metrics import mean_squared_error,r2_score
 from math import sqrt    
 
 data = pd.read_csv("C:/Users/admin/Documents/Fanny Mayorga/Python/csv/queue_calls.csv")
 
 data['queue'] = data['queue'].replace(('Servicios Preferenciales','H_EAV','H_CCC','H_CIT','H_INF','H_AMB','H_MAD','M_UIO','H_HOS','H_ACT','H_DEN','M_GYE','H_VEN','M_SAC','M_COR','H_ECA'),1)
-
-data['time'] = data['time'].astype("datetime64[ns]")
-data['time'] = data['time'].map(lambda x: x.replace(second=0, minute=0))
+data['date'] = data['date'].astype("datetime64[ns]")
 data['queue'] = data['queue'].astype("int64")
-
- 
+data['time'] = data['time'].astype("datetime64[ns]")
+data['time'] = data['time'].dt.hour
 data = data.groupby(['date','time']).sum()
 
 data.fillna(0, inplace=True)
@@ -30,23 +28,21 @@ before_rows - afther_rows
 
 data.reset_index(level=0, inplace=True)
 
-mask1 = data['date'] > '2017-01-01'
-mask2 = data['date'] < '2019-03-27' 
+"""mask1 = data.index < '2019-01-01'  
+mask2 = data.index < '2019-03-27' """
 
-data = data[mask1 & mask2]
+#data = data[mask1 & mask2]
 
-data['date'] = pd.to_datetime(data['date'])
 data['date'] = data['date'].astype("int64")
 
-print(data.dtypes)
-
 features = ['queue']
+
 y = data[features]
+
 target = ['date']
+
 x = data[target]
 
-print(x.head())
-print(x.shape,y.shape)
 X_train, X_test ,  y_train , y_test = train_test_split(x,y , test_size=0.20, random_state=42)
 future = LinearRegression()
 future.fit(X_train, y_train)
@@ -55,19 +51,21 @@ y_prediction = future.predict(X_test).astype("int64")
 
 RMSE = sqrt(mean_squared_error(y_true= y_test,y_pred= y_test)) 
 
+
 m = future.coef_[0]
 b = future.intercept_
 y_m = m*x+b
-print(y_prediction[:10])
-
 
 data['date'] = data['date'].astype("datetime64[ns]")
-print(data.head(11))
+
+print(y_prediction[:10])
+print(data.head(9))
 
 plt.scatter(x,y)
 plt.plot(x,y_m, color='red')
-plt.xlabel('date')
+plt.xlabel('time')
 plt.ylabel('queue')
 plt.title('calls per days')
 plt.grid()
 plt.show()
+
